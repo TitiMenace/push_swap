07dc8f95b2f706ecb5030438de46fc02e0ded054
Tian SCHECROUN <tschecro@e2r1p3.clusters.42paris.fr>
2023-02-25 00:25:25 +0100

    singletone issue

--- ps_algorithms.c
+++ ps_algorithms.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   push_swap.c                                        :+:      :+:    :+:   */
+/*   ps_algorithms.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: tschecro <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/02/24 18:47:53 by tschecro          #+#    #+#             */
-/*   Updated: 2023/02/24 22:53:27 by tschecro         ###   ########.fr       */
+/*   Created: 2023/02/24 22:08:34 by tschecro          #+#    #+#             */
+/*   Updated: 2023/02/24 22:52:22 by tschecro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,40 +14,18 @@
 #include "includes.h"
 #include "ft_push_swap.h"
 
-int	push_swap(int ac)
-{
-	if (ac == 4)
-		three_params();
-	return (1);
-}
-
-
-int	main(int ac, char **av)
+int	three_params(void)
 {
 	t_data	*data;
-	int		i;
 
-	if (ac < 2)
-	{
-		write(1,"Missing Args !", 14);
-		return (-1);
-	}
-	init_struct(ac, av);
 	data = _data();
-	data->stack_a = init_stack(ac, av);
-	if (!data->stack_a)
-	{
-		write(1, "Wrong Args !", 12);
-		return (-1);
-	}
-	data->len_a = ac - 1;
-	i = 0;
-	push_swap(ac);
-	while (i < ac - 1)
-	{
-		ft_putnbr(data->stack_a[i]);
-		write(1, " ", 1);
-		i++;
-	}
+	if (data->stack_a[0] > data->stack_a[1] && data->stack_a[1] < data->stack_a[2])
+		rotate_a(data);
+	if (data->stack_a[0] < data->stack_a[1])
+		swap_a(data);
+	if (data->stack_a[1] > data->stack_a[2])
+		reverse_rotate_a(data);
+	if (data->stack_a[0] > data->stack_a[1])
+		swap_a(data);
 	return (1);
 }
